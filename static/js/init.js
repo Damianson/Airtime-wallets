a31f67f0156a4780c40df6fabe6dc004300869c8
Damianson <damiansonuchegbu2017@gmail.com>
2020-06-20 23:09:49 +0100

    corrected all bugs and created return response
    the airtime buy endpoint is now responsive

--- static/js/init.js
+++ static/js/init.js
@@ -10,3 +10,4 @@ $(document).ready(function () {
   $('select').formSelect();
 });
 
+
--- wallet.py
+++ wallet.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask import request
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -15,11 +17,25 @@ def result():
     amount = request.form['amount']
     number = request.form['number']
 
-    return (request.form['airtime'])
+    url = 'https://api.wallets.africa/bills/airtime/purchase'
+    headers = {'Authorization': 'Bearer ' + publicKey}
+    payload = {'Code': str(airtime),
+               'Amount': int(amount),
+               'PhoneNumber': str(number),
+               'SecretKey': str(secretKey)}
+    r = requests.post(url, headers=headers, json=payload)
+    feedback = r.json()
+    code_back = feedback['ResponseCode']
+    message = feedback['Message']
+    
+    if code_back == 100:
+        return render_template('success.html', message=message)
+    else:
+        return render_template('fail.html', message=message)
 
 @app.route('/form')
 def form():
     return render_template('form.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
